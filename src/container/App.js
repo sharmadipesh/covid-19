50781eef34fbd2b4d3b84ea5fed2dff108517641
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-04-04 16:33:10 +0530

    [PACKAGE] Node-sass 📦

---
+++ src/container/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>
+          Hello Covid-19
+        </h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;

17626158859d59e3011b066f753b079334e8150e
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-04-05 15:47:49 +0530

    [SETUP] Antd design

--- src/container/App.js
+++ src/container/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <center>
         <h1>
-          Hello Covid-19
+          Hello Covid
         </h1>
       </center>
+      <Button type="primary">Button</Button>
     </div>
   );
 }

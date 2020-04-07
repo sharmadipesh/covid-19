1ec4fcb088c6f13097e29936d8e6a9e3c244fac6
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-04-07 18:56:38 +0530

    [SETUP] Fonts and Theme Setup

--- src/container/App.js
+++ src/container/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { Button } from 'antd';
+import React,{Component} from 'react';
 import 'styles/index.scss';
+import Dashboard from 'pages/dashboard';
 
 
-function App() {
-  return (
-    <div className="App">
-      <center>
-        <h1>
-          Hello Covid
-        </h1>
-      </center>
-      <Button type="primary">Button</Button>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dashboard {...this.props} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/pages/dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <div className="ptb-20 plr-30">
+                    <div>
+                        <img alt="logo" id="loading" src="images/virus.svg" height="65px" width="65px"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;

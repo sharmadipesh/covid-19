95639a67076816ced0e990faf8635948297ece65
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-04-08 18:42:36 +0530

    [SETUP] Bootstrap

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+// import { Card } from 'antd';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <div className="ptb-20 plr-30">
-                    <div>
-                        <img alt="logo" id="loading" src="images/virus.svg" height="65px" width="65px"/>
+                <div className="header-container">
+                    <div className="parent-row parent-v-center">
+                        <img alt="logo" id="loading" src="images/virus.svg" className="page-logo mr-10"/>
+                        <div className="parent-row parent-v-center">
+                            <div className="header-title text-color-white">COVID - </div>
+                            <div className="header-title color-text-green">19</div>
+                        </div>
                     </div>
+                </div>
+                {/* worldwide container */}
+                <div className="mtb-20 parent-row parent-h-center world-wide-container">
+                    {/* <Card className="card-styling">
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card> */}
                 </div>
             </div>
         );

7bc3fb1edaa2b64d5294fe3a4fd2f7c4967df057
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-04-10 00:25:53 +0530

    [BUILD] Worldwide

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -6,21 +6,29 @@ class Dashboard extends Component {
         return (
             <div>
                 <div className="header-container">
-                    <div className="parent-row parent-v-center">
+                    <div className="d-flex align-items-center">
                         <img alt="logo" id="loading" src="images/virus.svg" className="page-logo mr-10"/>
-                        <div className="parent-row parent-v-center">
+                        <div className="d-flex align-items-center">
                             <div className="header-title text-color-white">COVID - </div>
                             <div className="header-title color-text-green">19</div>
                         </div>
                     </div>
                 </div>
                 {/* worldwide container */}
-                <div className="mtb-20 parent-row parent-h-center world-wide-container">
-                    {/* <Card className="card-styling">
-                        <p>Card content</p>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                    </Card> */}
+                <div className="mtb-10 d-flex flex-nowrap world-wide-container">
+                    <div className="card-style card-death">
+                        {/* blur */}
+                        <span className="blur-effect" />
+                    </div>
+                    <div className="card-style card-confirmed-cashes">
+                        {/* blur */}
+                        <span className="blur-effect" />
+                    </div>
+                    <div className="card-style card-recovery">
+                        {/* blur */}
+                        <span className="blur-effect" />
+                    </div>
+                    <div style={{color:'transparent'}}>In</div>
                 </div>
             </div>
         );
